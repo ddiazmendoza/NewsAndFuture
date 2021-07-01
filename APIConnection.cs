c12fa4dfc0673c8a658949d22cf02b69eea1d41f
Diego D <56505625+ddiazmendoza@users.noreply.github.com>
2021-06-30 23:02:55 -0500

    control

--- APIConnection.cs
+++ APIConnection.cs
@@ -8,7 +8,42 @@ namespace NewsAndFuture
 {
     public class APIConnection
     {
-        public static async Task<ArticlesRoot> EstablishConnectionAsync(string search, string language)
+        public static async Task<ArticlesRoot> GetTopHeadlines (string country) 
+        {
+            var url = "http://newsapi.org/v2/top-headlines?" +
+            $"country={country}&" +
+            "pageSize=100&" +
+            "apiKey=fb86b8988414247fb9b0000140cc3838c";
+
+            var myClient = new HttpClient() {BaseAddress = new Uri(url)};
+
+            try
+            {
+                var request = await myClient.GetAsync(myClient.BaseAddress);
+
+                var myModel = new ArticlesRoot();
+                
+
+                if (request.IsSuccessStatusCode)
+                {
+                    var content = await request.Content.ReadAsStringAsync();             
+                    var model = JsonSerializer.Deserialize<ArticlesRoot>(content, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    System.Console.WriteLine(model.Status + " Artículos encontrados: " +  model.Articles.Count);
+                    myModel = model;
+                }
+                else
+                {
+                    Console.WriteLine("Request error");
+                }
+                return myModel;
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public static async Task<ArticlesRoot> Search(string search, string language)
             {
                 var q = search;
                 var lang = language;
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -27,7 +29,7 @@ namespace NewsAndFuture.Pages
         {
             try
             {
-                var articles = await APIConnection.EstablishConnectionAsync(search, lang);
+                var articles = await APIConnection.Search(search, lang);
                 var _art = articles.Articles;
                 return _art;    
             }
@@ -38,6 +40,47 @@ namespace NewsAndFuture.Pages
             }
 
         }
-       
+        
+        public static async Task<List<Article>> GetTopHeadlines() 
+        {
+            try
+            {
+                var ArticlesRoot = await APIConnection.GetTopHeadlines("mx");
+                var Articles = ArticlesRoot.Articles;
+                System.Console.WriteLine($"Artículos top headlines mex encontrados: {Articles.Count}");
+                return Articles;
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return null;
+            }
+            
+        }
+         public static async Task<Currency> GetCurrencies(string SEARCH, string USDorBTC) 
+        {
+
+            var sochainClient = new HttpClient() {BaseAddress = new Uri($@"https://sochain.com/api/v2/get_price/{SEARCH}/{USDorBTC}")};
+            try
+            {
+                var request = await sochainClient.GetAsync(sochainClient.BaseAddress);
+                if (request.IsSuccessStatusCode)
+                {
+                    var content = await request.Content.ReadAsStringAsync();
+                    var deserializedContent = System.Text.Json.JsonSerializer.Deserialize<Currency>(content, new JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+                    return deserializedContent;
+                } 
+                else 
+                {
+                    System.Console.WriteLine("error wey");
+                    return null;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
     }
 }
